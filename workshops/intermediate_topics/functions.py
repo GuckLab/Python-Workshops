365fa79ea0df27604a931b9d798afb713c719188
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2023-01-18 15:08:22 +0100

    enh: fill in the functions basic

---
+++ workshops/intermediate_topics/functions.py
@@ -0,0 +1,32 @@
+"""
+Python Functions
+"""
+
+
+def add_two_numbers_1(a, b):
+    sum_value = a + b
+    return sum_value
+
+
+def add_two_numbers_2(a, b):
+    """Briefly describe what the function does."""
+    sum_value = a + b
+    return sum_value
+
+
+def add_two_numbers_3(a, b):
+    """Briefly describe what the function does.
+
+    Parameters
+    ----------
+    a, b : int or float
+        Describe the parameters
+
+    Returns
+    -------
+    sum_value : float
+        Describe what is returned by the function
+
+    """
+    sum_value = a + b
+    return sum_value
