886bc688a12ccb77ce68d58ff72754ed32e6469c
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2023-01-27 14:23:53 +0100

    enh: add pytest workshop example

--- workshops/intermediate_topics/functions.py
+++ workshops/intermediate_topics/functions.py
@@ -1,32 +1,3 @@
 """
-Python Functions
+See Notebook "Python-Intermediate-Workshop-Functions.ipynb"
 """
-
-
-def add_two_numbers_1(a, b):
-    sum_value = a + b
-    return sum_value
-
-
-def add_two_numbers_2(a, b):
-    """Briefly describe what the function does."""
-    sum_value = a + b
-    return sum_value
-
-
-def add_two_numbers_3(a, b):
-    """Briefly describe what the function does.
-
-    Parameters
-    ----------
-    a, b : int or float
-        Describe the parameters
-
-    Returns
-    -------
-    sum_value : float
-        Describe what is returned by the function
-
-    """
-    sum_value = a + b
-    return sum_value
---
+++ workshops/intermediate_topics/unit_testing_pytest.py
@@ -0,0 +1,64 @@
+"""
+# Unit testing with pytest
+
+A unit test is a test on one part of your code. Ideally you are testing a
+function or method.
+The next level would be system tests that test many aspects of a program together.
+
+We use the popular `pytest` package for testing.
+
+## Why do we need to write tests?
+
+We write tests so that we know that our code works as expected.
+With tests we can check that the actual output of our code is what we expect.
+
+## Installing pytest
+
+`pip install pytest`
+
+## Using pytest
+
+`pytest path/to/file.py`
+
+We can also run it on multiple files or directories:
+
+`pytest path/to/directory path/to/file.py`
+
+The tests are usually in a separate directory from the package code.
+
+## Arrange, Act, Assert
+
+The test is split up into:
+- Arrange: we set up any variables we eed, including the expected answer.
+- Act: Run the code that is being tested.
+- Assert: use the python `assert` statement to check if the actual and expected
+  output are the same.
+
+"""
+
+
+# we write tests by simply creating a function that starts with "test"
+# here is a simple function
+
+def add_some_numbers(number1, number2):
+    addition = number1 + number2
+    return addition
+
+
+# we test this function with a test function like so:
+
+def test_add_some_numbers_int():
+    """Test the addition of two integers."""
+    # arrange
+    number1 = 1
+    number2 = 2
+    expected_value = 3
+
+    # act
+    addition = add_some_numbers(number1, number2)
+
+    # assert
+    assert addition == expected_value
+
+
+# now run `pytest workshops/intermediate_topics/unit_testing_pytest.py`
