737f977d8a71bbfc419c9a4f380b18488a2fb41d
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2023-06-19 16:53:27 +0200

    update beginner nb structure

---
+++ workshops/beginner_topics/importing_modules.py
@@ -0,0 +1,14 @@
+
+import dclab
+
+import numpy as np
+import pandas as pd
+
+from skimage import gaussian_filter
+from skimage import (
+	gaussian_filter, other_filter,
+	other_filter2, other_filter3
+)
+
+
+
